6f3ba2695c779e1c2106154525133dfdec0c6b73
Ariel Alejandro Reyes Cruz <arialex02@live.com.mx>
2021-08-30 18:38:08 -0500

    Completé el ejercicio: Videojuegos

--- assignments/10Videojuego/src/exercise.py
+++ assignments/10Videojuego/src/exercise.py
@@ -1,6 +1,10 @@
 def main():
-    # escribe tu código abajo de esta línea
-    pass
+    juegosNuevos = int(input("Dame la cantidad de juegos nuevos: "))
+    juegosUsados = int(input("Dame la cantidad de juegos usados: "))
+
+    precioTotal = (juegosNuevos * 1000) + (juegosUsados * 350)
+
+    print("El total de la compra es:", precioTotal)
 
 
 if __name__ == '__main__':
