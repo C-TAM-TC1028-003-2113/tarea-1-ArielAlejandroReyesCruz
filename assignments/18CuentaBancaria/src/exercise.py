99445d9a2e4311e8456503bf4b282f7621dc63d8
Ariel Alejandro Reyes Cruz <arialex02@live.com.mx>
2021-08-30 18:55:03 -0500

    Completé el ejercicio: Cuenta Bancaria

--- assignments/18CuentaBancaria/src/exercise.py
+++ assignments/18CuentaBancaria/src/exercise.py
@@ -1,6 +1,13 @@
 def main():
-    #Placeholder
-    pass
+    saldoMesAnterior = float(input("Dame el saldo del mes anterior: "))
+    ingresos = float(input("Dame los ingresos: "))
+    egresos = float(input("Dame los egresos: "))
+    cheques = int(input("Dame el número de cheques: "))
+
+    saldoSinIva = (saldoMesAnterior + ingresos - egresos - (cheques * 13))
+    saldoConIva = saldoSinIva * 0.925
+
+    print("El saldo final de la cuenta es:", saldoConIva)
 
 if __name__ == '__main__':
     main()
