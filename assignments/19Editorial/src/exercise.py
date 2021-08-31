a99df8319a1cf0b4c0ce5746aaf6f1c15c5a1157
Ariel Alejandro Reyes Cruz <arialex02@live.com.mx>
2021-08-31 02:41:37 -0500

    Completé el ejercicio: Editorial

--- assignments/19Editorial/src/exercise.py
+++ assignments/19Editorial/src/exercise.py
@@ -1,6 +1,11 @@
+import math
 def main():
-    # escribe tu código abajo de esta línea
-    pass
+    palabras = int(input("Dame el número de palabras: "))
+
+    numPaginas = math.ceil(palabras/475)
+    costo = (numPaginas*60)*0.90
+
+    print("El costo de la publicación es:", costo)
 
 
 if __name__ == '__main__':
